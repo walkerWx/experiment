25f07064edd40ccf5b797255f6bf2f3672cf1f72
Walker <walkernju@gmail.com>
2017-10-26 07:16:55 -0700

    generate input

--- 1/src/stableAnalysis.py
+++ 1/src/stableAnalysis.py
@@ -4,7 +4,7 @@
 import json
 
 # 以step为步长对输入区间[start, end]进行划分，并将结果输出 
-def divideInputSpace(varNum, start, end, prec, outputFile):
+def divideInputSpace(varNum, start, end, prec):
 
     d1 = []
     dn = []
@@ -24,23 +24,51 @@ def divideInputSpace(varNum, start, end, prec, outputFile):
         dn = list(dt)
 
 
+    '''
     output = {}
     output['varNum'] = varNum 
     output['intervals'] = dn
 
     with open(outputFile, 'w') as f:
         json.dump(output, f, indent=4)
+    '''
+
+    return dn
 
 
+# 计算每个变量的范围表示的N维空间的所有顶点，例如 x = [1, 2], y = [2, 4] 对应二维空间中的4个顶点 (1, 2) (1, 4) (2, 2) (2, 4)
+def interval2Points(interval):
+
+    varNum = len(interval)
+    points = [[interval[0][0]], [interval[0][1]]]
+    i = 1
+    while (i < len(interval)):
+        t = []
+        for point in points:
+            t.append(point + [interval[i][0]])
+            t.append(point + [interval[i][1]])
+        points = list(t)
+        i += 1
+        
+    return points
 
 
+def stableAnalysis(varNum, path, constrain):
+    
+    start = 0
+    end = 1
+    prec = 2 # step = 1e-2 
 
+     
+    intervals = divideInputSpace(varNum, start, end, prec)
+    points = [interval2Points(interval) for interval in intervals]
+     
 
+    for i in range(len(intervals)):
+        print (intervals[i])
+        print (points[i])
+    
+    
 
+stableAnalysis(2,2,2)
 
-
-
-
-
-
-def stableAnalysis()
