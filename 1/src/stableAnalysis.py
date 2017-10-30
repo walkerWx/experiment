d1b085b680c5382a19ddd970bfa5ac69ea111485
wangxie <walkernju@gmail.com>
2017-10-30 13:16:03 +0800

    move generateCpp implement to stableAnalysis

--- 1/src/stableAnalysis.py
+++ 1/src/stableAnalysis.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 
+from __future__ import print_function
+
 import json
+
+FLOATCPP = 'float.cpp'
+REALCPP = 'real.cpp'
 
 # 以step为步长对输入区间[start, end]进行划分，并将结果输出 
 def divideInputSpace(varNum, start, end, prec):
@@ -52,23 +57,92 @@ def interval2Points(interval):
         
     return points
 
+# generate runable cpp file according to path, constrain and type
+def generateCpp(variables, path, constrain, type = 'all'):
+    
+    if (type == 'all'):
+        generateCpp(variables, path, constrain, 'float')
+        generateCpp(variables, path, constrain, 'real')
+        return
+        
+    # according to different implement type, we should include different header files and use different things
 
-def stableAnalysis(varNum, path, constrain):
+    header = ''
+    declType = ''
+    inputStream = ''
+    outputStream = ''
+    precisionSetting = ''
+
+
+    if (type == 'float'):
+
+        header += '#include <iostream>\n'
+        header += '#include <iomanip>\n'
+        header += '#include <cmath>\n'
+        header += '#include <limits>\n'
+        header += 'using namespace std;\n'
+
+        declType = 'double'
+        inputStream = 'cin'
+        outputStream = 'cout'
+
+        precisionSetting = outputStream + ' << scientific << setprecision(numeric_limits<double>::digit10)\n'
+
+    elif (type == 'real'):
+
+        header += '#include "iRRAM.h"\n'
+        header += 'using namespace iRRAM;\n'
+
+        declType = 'REAL'
+        inputStream = 'cin'
+        outputStream = 'cout'
+
+        precisionSetting = outputStream + ' << setRwidth(45)\n'
+
+    elif (type == 'interval'):
+        # TODO
+        header += ''
+        declType = ''
+
+    mainFunc = ''
+    mainFunc += 'int main(){\n'
+    mainFunc += '\t' + precisionSetting
+    mainFunc += '\t' + declType + ','.join(variables) + ';\n'
+    mainFunc += '\t' + inputStream + ' >> ' + ' >> '.join(variables) + ';\n'
+    mainFunc += '\t' + declType + ' res;\n'
+    mainFunc += '\t' + 'res = ' + path + ';\n'
+    mainFunc += '\t' + outputStream + ' << res;\n'
+    mainFunc += '}\n'
+
+    outputFile = {}
+    outputFile['float'] = FLOATCPP
+    outputFile['real'] = REALCPP 
+    f = open(outputFile[type], 'w')
+    print (header, file=f)
+    print (mainFunc, file=f)
+
+# 稳定性分析主逻辑
+def stableAnalysis(variables, path, constrain):
     
     start = 0
     end = 1
-    prec = 2 # step = 1e-2 
-
+    prec = 3 # step = 1e-2 
      
-    intervals = divideInputSpace(varNum, start, end, prec)
+    '''
+    intervals = divideInputSpace(len(varNum), start, end, prec)
     points = [interval2Points(interval) for interval in intervals]
-     
 
     for i in range(len(intervals)):
         print (intervals[i])
         print (points[i])
-    
+    '''
+
+    generateCpp(variables, path, constrain)
+     
     
 
-stableAnalysis(2,2,2)
+variables = ['x', 'y']
+path = 'sin(x)*cos(y)'
+constrain = '0<x&&x<1&&0<y&&y<1'
+stableAnalysis(variables, path, constrain)
 
