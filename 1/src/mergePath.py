12b7462cf0555a3ada9384bbe691e67d4401ee26
Walker <walkernju@gmail.com>
2017-11-04 23:20:19 -0700

    finish mergepath implemetion

--- 1/src/mergePath.py
+++ 1/src/mergePath.py
@@ -1,43 +1,106 @@
 
 from __future__ import print_function
+from transform import convertPath
 
 import json
 
+# TODO
+# different type implemention && loop implemention
 # merge paths in pth file to a C program
 def mergePath(pthFile):
 
     with open(pthFile) as f:
         data = json.load(f)
 
-        outputDirectory = ''
-        if (pthFile.rfind('/') != -1):
-            outputDirectory = pthFile[:pthFile.rfind('/')+1]
+    variables = data['variables']
+    types = data['types']
+    paths = data['paths']
+    constrains = data['constrains']
 
-        outputFile = open(outputDirectory+data['programName']+'_o.cc', 'w')
-    
-        # function declaration
-        print ('double ' + data['functionName'], end='', file=outputFile)        
+    header = '''
+#include "iRRAM.h"
+#include <iostream>
+#include <iomanip> 
+#include <cmath>
+#include <limits>
+using namespace std;
+using namespace iRRAM;
+             '''
+            
+    declType = ''
+    returnType = ''
+    if ('real' in data['types']):
+        declType = 'REAL'
+        returnType = 'REAL'
+    else:
+        declType = 'double'
+        returnType = 'double'
 
-        # construct argument list
-        for i in range(len(data['variables'])):
-            data['variables'][i] = 'double '+data['variables'][i]
-        argumentList = '('+' ,'.join(data['variables'])+')'
-        print (argumentList, file=outputFile)
+    # function declare
+    funcBody = returnType + ' ' + data['functionName'] + '(' + ', '.join([declType+' '+v for v in data['variables']]) +  ') { \n'
 
-        # function body
-        print ('{', file=outputFile)
+    # double variables declare and initialize
+    variables_double = [v + '_double' for v in data['variables']]
+    funcBody += '\tdouble ' + ', '.join(variables_double) + ';\n'
+    for i in range(len(variables)):
+        if (declType == 'double'):
+            funcBody += '\t' + variables_double[i] + ' = ' + variables[i] + ';\n'
+        elif (declType == 'REAL'):
+            funcBody += '\t' + variables_double[i] + ' = ' + variables[i] + '->as_double();\n'
+    funcBody += '\n'
+         
+    # real variables declare and initialize
+    variables_real = [v + '_real' for v in data['variables']]
+    funcBody += '\tREAL ' + ', '.join(variables_real) + ';\n'
+    for i in range(len(variables)):
+        funcBody += '\t' + variables_real[i] + ' = ' + variables[i] + ';\n'
+    funcBody += '\n'
+
+    # return value declare
+    funcBody += '\t' + 'double res_double;\n'
+    funcBody += '\t' + 'REAL res_real;\n'
+    funcBody += '\n'
         
-        # return value declaration
-        print ('\tdouble res = 0;', file=outputFile)
-        for i in range(len(data['constrains'])):
-            print ('\tif('+data['constrains'][i]+')', file=outputFile)
-            print ('\t{', file=outputFile)
-            print ('\t\t res = ' + data['paths'][i] + ';', file=outputFile)
-            print ('\t}', file=outputFile)
-
-        # return statement
-        print ('\treturn res;', file=outputFile)
-
-        print ('}', file=outputFile)
+    # paths implemention
+    for i in range(len(data['paths'])):
 
 
+        funcBody += '\tif(' + constrains[i] + ') {\n' 
+
+        # float implemention
+        if (types[i] == 'float'):
+            funcBody += '\t\tres_double  = ' + convertPath(paths[i], variables, variables_double) + ';\n'
+
+            # return statement
+            if (returnType == 'double'):
+                funcBody += '\t\treturn res_double;\n'
+            elif (returnType == 'REAL'):
+                funcBody += '\t\treturn REAL(res_double);\n'
+
+        # real implemention
+        elif (types[i] == 'real'):
+            funcBody += '\t\tres_real = ' + convertPath(paths[i], variables, variables_real)  + ';\n'
+
+            # return statement
+            if (returnType == 'double'):
+                funcBody += '\t\treturn res_real->as_double();\n' 
+            elif (returnType == 'REAL'):
+                funcBody += '\t\treturn res_real;\n' 
+
+        funcBody += '\t}\n\n'
+
+    funcBody += '\treturn 0;\n'
+    funcBody += '}\n'
+
+    # output to file
+    outputDirectory = ''
+    if (pthFile.rfind('/') != -1):
+        outputDirectory = pthFile[:pthFile.rfind('/')+1]
+
+    outputFile = open(outputDirectory+data['programName']+'_o.cc', 'w')
+
+    print (header, file=outputFile)
+    print (funcBody, file=outputFile)
+
+
+mergePath('../case/analytic/analytic.opt.pth')
