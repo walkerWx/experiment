417cbc824b22afd5a8f4f81d00fa2afb5368dd69
Wang Xie <290113699@qq.com>
2018-05-05 21:40:33 +0800

    finish command line tool for generate points

--- 2/src/herbie_report.py
+++ 2/src/herbie_report.py
@@ -1,103 +1,9 @@
 import os
-import re
 
 
-class HerbieReport:
+# 找到所有的compiled.c文件，也就是包含了Herbie优化后程序代码的文件，将其中优化后程序组成一个C文件
+def generate_herbie_cc():
 
-    '''
-        Herbie报告，包含了所有的Herbie用例
-    '''
-
-    def __init__(self, dir):
-        for root, dirs, files in os.walk(dir):
-            for name in files:
-                if name == 'graph.html':
-                    case_file = os.path.join(root, name)
-                    hc = HerbieCase(case_file)
-
-        return
-
-
-class HerbieCase:
-
-    '''
-        Herbie用例，包含了用例名称，原始计算式，转换后计算式等信息
-    '''
-
-    def __init__(self, file):
-
-        self.case_name = ''  # 用例名称
-        self.origin_expr = ''  # 原始计算式
-        self.transformed_expr = list()  # 转换后计算式
-        self.constrain_expr = list()  # 转换后计算式对应约束，len(transformed_expr) = len(constrain_expr) + 1，最后有else情况
-
-        try:
-            report_file = open(file, 'r', encoding='utf-8')
-        except IOError:
-            print("[Error] Can't Open file " + file + '. Exit.')
-            exit()
-
-        report_content = report_file.read()
-
-        case_name_pattern = re.compile(r'results for ([a-z0-9]+)', re.I)
-
-        try:
-            m = case_name_pattern.search(report_content)
-            self.case_name = m.group(1)
-        except AttributeError:
-            print("[Error] Can't find case name. Exit.")
-            exit()
-
-        origin_expr_pattern = re.compile(r'<div class=\'program\'>(.*?)</div>', re.S)
-        try:
-            m = origin_expr_pattern.findall(report_content)
-            self.origin_expr = m[0]
-            self.transformed_expr = m[1]
-        except AttributeError:
-            print("[Error] Cant't find origin expression. Exit.")
-
-        # 对提取的字符串做些预处理
-        self.origin_expr = self.origin_expr.replace('\[', '')
-        self.origin_expr = self.origin_expr.replace('\]', '')
-
-        self.transformed_expr = self.transformed_expr.replace('\[', '')
-        self.transformed_expr = self.transformed_expr.replace('\]', '')
-        self.transformed_expr = self.transformed_expr.replace('\\begin{array}{l}', '')
-        self.transformed_expr = self.transformed_expr.replace('\\end{array}', '')
-        self.transformed_expr = self.transformed_expr.replace('\\;', '')
-        self.transformed_expr = self.transformed_expr.replace('\\\\', '')
-        self.transformed_expr = self.transformed_expr.replace('\\cdot', '*')
-        self.transformed_expr = self.transformed_expr.replace('\\left', '')
-        self.transformed_expr = self.transformed_expr.replace('\\right', '')
-
-        transformed = [x for x in self.transformed_expr.splitlines() if not re.match(r'^\s*$', x)]  # 去掉空行
-
-        self.transformed_expr = list()
-        self.constrain_expr = list()
-
-        if len(transformed) == 1:
-            self.transformed_expr.extend(transformed)
-            return
-        else:
-            for line in transformed:
-                if line.startswith('\\mathbf{if}') and line.endswith(':'):
-                    self.constrain_expr.append(line[11:-1])
-                elif line.startswith('\\mathbf{else}:'):
-                    continue
-                else:
-                    self.transformed_expr.append(line)
-
-        print('Case name:' + '\t'*5 + self.case_name)
-        print('Origin expression:' + '\t'*3 + self.origin_expr)
-        print('Transformed expression:')
-        print('\n'.join(self.transformed_expr))
-        print('Constrain expression:')
-        print('\n'.join(self.constrain_expr))
-
-
-if __name__ == "__main__":
-
-    # 找到所有的compiled.c文件，也就是包含了Herbie优化后程序代码的文件，将其中优化后程序组成一个C文件
     compiled_files = list()
     report_dir = '/Users/walker/Desktop/HerbieCases'
     for root, dirs, files in os.walk(report_dir):
@@ -125,3 +31,22 @@ if __name__ == "__main__":
     herbie_file.close()
 
 
+def prepare(casename):
+
+    casedir = "../case/" + casename
+    os.makedirs(casedir, exist_ok=True)
+
+    # 准备irram可执行文件
+    irram_file = os.path.join(casedir, 'irram.cc')
+    print(irram_file)
+
+
+    # 准备herbie可执行文件
+    herbie_file = os.path.join(casedir, 'herbie.cc')
+
+    # 准备随机浮点数
+    points_file = os.path.join(casedir, 'points.txt')
+
+
+if __name__ == "__main__":
+    prepare("sintan")
